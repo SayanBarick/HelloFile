79435d3c0d95d3a8acf6eecc88e00b69d1df1ae0
sayan barick <sayanbarick2000@gamil.com>
2026-05-05 20:58:23 +0530

    6 edited

--- HelloWorld2.java
+++ HelloWorld2.java
@@ -5,10 +5,7 @@ class HelloWorld2
 		System.out.println("Hello World");
 		System.out.println("Hello World2 added");
 		System.out.println("Hello World3 added");
-<<<<<<< HEAD
 		System.out.println("Hello World5 added");
-=======
->>>>>>> 37ca156d6c6590890d89891486c2b29509c78f6f
-		System.out.println("Hello World6 added");
+		System.out.println("Hello World7 added");
 	}
 }
