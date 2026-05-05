5259cf55582d36ec0a89485c9c4a6edd1472cee3
sayan barick <sayanbarick2000@gamil.com>
2026-05-05 20:33:37 +0530

    3 added

--- HelloWorld2.java
+++ HelloWorld2.java
@@ -3,5 +3,7 @@ class HelloWorld2
 	public static void main(String args[])
 	{
 		System.out.println("Hello World");
+		System.out.println("Hello World2 added");
+		System.out.println("Hello World3 added");
 	}
 }
